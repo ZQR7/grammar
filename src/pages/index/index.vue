86d6059bcf4b0a699295e93064cc1b845fc7c64d
ZQR7 <2397721424@qq.com>
2019-11-30 18:03:33 +0800

    new

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-     <i-grid i-class="no-border">
+    <swiper
+      :indicator-dots="indicatorDots"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+    >
+      <block v-for="img in imgUrls" :key="img">
+        <swiper-item>
+          <image :src="img" style="width:100%" />
+        </swiper-item>
+      </block>
+    </swiper>
+    <i-grid i-class="no-border">
     <i-grid-item @click="goType(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
         <i-grid-icon>
             <image :src="grid.image" />
@@ -8,26 +20,14 @@
         <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item>
   </i-grid>
-
-   <i-grid i-class="no-border">
-    <i-grid-item @click="goType(krid)" v-for="krid in krids" :key="krid" i-class="no-border">
-        <i-grid-icon>
-            <image :src="krid.image" />
-        </i-grid-icon>
-        <i-grid-label>{{krid.title}}</i-grid-label>
-    </i-grid-item>
-  </i-grid>
-  
-   <i-grid i-class="no-border">
-    <i-grid-item @click="goType(crid)" v-for="crid in crids" :key="crid" i-class="no-border">
-        <i-grid-icon>
-            <image :src="crid.image" />
-        </i-grid-icon>
-        <i-grid-label>{{crid.title}}</i-grid-label>
-    </i-grid-item>
-  </i-grid>
-
-
+  <i-panel :title="title_name">
+    <view style="padding: 15px;">
+      <i-card v-for="item in top" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
+          <view slot="content">{{item.remark}}</view>
+          <view slot="footer">{{item.address}}</view>
+      </i-card>
+    </view>
+  </i-panel>
   </div>
 </template>
 
@@ -35,34 +35,28 @@
 export default {
   data () {
     return {
-      
+
       grids: [
         {title:"定语",image:"/static/images/1.png"},
         {title:"状语",image:"/static/images/2.png"},
         {title:"倒装句",image:"/static/images/3.png"},
         {title:"强调句",image:"/static/images/4.png"}
       ],
-
-      krids: [
-        {title:"名词",image:"/static/images/5.png"},
-        {title:"连词",image:"/static/images/6.png"},
-        {title:"动词",image:"/static/images/7.png"},
-        {title:"情态动词",image:"/static/images/8.png"}
-      ],
-
       
-      crids: [
-        {title:"定语从句",image:"/static/images/9.png"},
-        {title:"主语从句",image:"/static/images/10.png"},
-        {title:"状语从句",image:"/static/images/11.png"},
-        {title:"宾语从句",image:"/static/images/12.png"}
+      imgUrls: [
+        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
       ],
-
+      indicatorDots: false,
+      autoplay: false,
+      interval: 5000,
+      duration: 1000
     }
   },
 
   methods: {
-     goType(type){
+    goType(type){
       console.log(type)
       let url = '../list/main?type=' + type.title
       mpvue.navigateTo({ url })
@@ -73,7 +67,6 @@ export default {
   }
 }
 </script>
-
 
 <style scoped>
 div >>> .no-border {
